89cd879eec8b7106e738ecd589d6f30eda7da80c
david942j <david942j@gmail.com>
2014-11-28 20:20:22 +0800

    register dir watch

--- socket/client/watcher.rb
+++ socket/client/watcher.rb
@@ -2,29 +2,44 @@
 require 'rubygems'
 require 'rb-inotify'
 require 'curb'
+require 'eventmachine'
 
 class Watcher
-  @notifier = {}
+  @@notifier = {}
   def self.register(dir)
     dir = File.expand_path(dir)
-    @notifier[dir] = INotify::Notifier.new
-    @notifier[dir].watch(dir, :create, :delete, :modify) do |event|
-      event.flags.each do |flag|
-        flag = flag.to_s
-        p flag
-        p event
-        puts case flag
-          when 'create' then "#{event.name} create"
-          when 'delete' then "#{event.name} delete"
-          when 'modify' then "#{event.name} change"
+    return false if @@notifier[dir] != nil
+    Thread.new do
+      @@notifier[dir] = INotify::Notifier.new
+      @@notifier[dir].watch(dir, :create, :delete, :modify) do |event|
+        if event.flags.include?(:isdir)
+          if event.flags.include?(:create)
+            Watcher.register(event.watcher.path+'/'+event.name)
+          end
+        end
+        event.flags.each do |flag|
+          if flag==:create
+
+          end
+          flag = flag.to_s
+          p "flag = #{flag}"
+          #p event
+          puts case flag
+            when 'create' then "#{event.name} #{event.watcher.path} create"
+            when 'delete' then "#{event.name} delete"
+            when 'modify' then "#{event.name} change"
+          end
         end
       end
+      @@notifier[dir].run
     end
-    @notifier[dir].run
   end
 end
 
 Watcher.register('.')
+
+while true do
+end
 =begin
 notifier = INotify::Notifier.new
 
